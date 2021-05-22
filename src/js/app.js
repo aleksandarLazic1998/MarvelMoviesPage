a01f57322e82c1fd35ffe8efa6113876961bd537
aleksandarLazic191277 <acafanto@gmail.com>
2021-05-22 18:41:00 +0200

    Refactured JavaScript

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+// Global variable because it is usen i more functions
+let clicked = false;
 // NavBar DropDown Menu function
 function navBar() {
   const navBtn = document.getElementById('drop-down-nav-btn');
@@ -47,17 +49,17 @@ function changeYear() {
 // For seting the year
 changeYear();
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This function will add liking and disliking functionality
+function liking(){
 // This function will change element styles to represent like and bookmark
 function likeBookmark(element) {
   let like = 'fa fa-heart-o';
   let bookmark = 'fa fa-bookmark-o';
   if (element.classList.value === like) {
-    console.log(element);
     element.style.color = 'red';
     element.classList.remove('fa-heart-o');
     element.classList.add('fa-heart');
   } else if (element.classList.value === bookmark) {
-    console.log(element);
     element.style.color = 'black';
     element.classList.remove('fa-bookmark-o');
     element.classList.add('fa-bookmark');
@@ -82,7 +84,6 @@ const icons = document.querySelectorAll('.icons');
 const like = document.querySelectorAll('i.fa-heart-o');
 const bookmark = document.querySelectorAll('i.fa-bookmark-o');
 
-let clicked = false;
 // For each icon on the screen 
 icons.forEach(icon => {
   icon.addEventListener('click', function (event) {
@@ -97,66 +98,73 @@ icons.forEach(icon => {
     }
   });
 }, false);
+}
+// For Liking and disliking
+liking();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-let moduleImage = document.querySelectorAll('.module-image');
-let imageButton = document.querySelectorAll('.image-btn');
-
-// This function will be called for images Event Listener and it will add class to image that is clicked and all buttons
-function openImage(event) {
-  let image = event;
-  image.classList.add('open');
-  for (let i = 0; i < imageButton.length; i++) {
-    imageButton[i].classList.add('open');
+// This function will add functionality for opening image modulo and closing it
+function modules(){
+  let moduleImage = document.querySelectorAll('.module-image');
+  let imageButton = document.querySelectorAll('.image-btn');
+  
+  // This function will be called for images Event Listener and it will add class to image that is clicked and all buttons
+  function openImage(event) {
+    let image = event;
+    image.classList.add('open');
+    for (let i = 0; i < imageButton.length; i++) {
+      imageButton[i].classList.add('open');
+    }
   }
-}
-
-// This function will be called for images Event Listener and it will remove all classes from image and buttons
-function closeImageClick(event) {
-  let image = event;
-  image.classList.remove('open');
-  for (let i = 0; i < imageButton.length; i++) {
-    imageButton[i].classList.remove('open');
+  
+  // This function will be called for images Event Listener and it will remove all classes from image and buttons
+  function closeImageClick(event) {
+    let image = event;
+    image.classList.remove('open');
+    for (let i = 0; i < imageButton.length; i++) {
+      imageButton[i].classList.remove('open');
+    }
   }
-}
-
-// This function will be called in Event Lisener for buttons when the x btn is clicked and when the Escape button was pressed 
-// It will close opened image and button
-function closeAll() {
-  for (let i = 0; i < imageButton.length; i++) {
-    imageButton[i].classList.remove('open');
+  
+  // This function will be called in Event Lisener for buttons when the x btn is clicked and when the Escape button was pressed 
+  // It will close opened image and button
+  function closeAll() {
+    for (let i = 0; i < imageButton.length; i++) {
+      imageButton[i].classList.remove('open');
+    }
+    for (let i = 0; i < moduleImage.length; i++) {
+      moduleImage[i].classList.remove('open');
+    }
   }
-  for (let i = 0; i < moduleImage.length; i++) {
-    moduleImage[i].classList.remove('open');
-  }
-}
-
-// Event Listener when you click the image
-imageButton.forEach(button => {
-  button.addEventListener('click', function () {
-    closeAll();
+  
+  // Event Listener when you click the image
+  imageButton.forEach(button => {
+    button.addEventListener('click', function () {
+      closeAll();
+    });
   });
-});
-
-// Event Listener when you press Escape key button 
-document.addEventListener('keyup', function (event) {
-  if (event.key === 'Escape') {
-    closeAll();
-  }
-});
-
-// Event Listener for Images
-moduleImage.forEach(images => {
-  images.addEventListener('click', function (event) {
-    if (clicked === false) {
-      openImage(event.target);
-
-      clicked = true;
-    } else {
-      closeImageClick(event.target);
-
-      clicked = false;
+  
+  // Event Listener when you press Escape key button 
+  document.addEventListener('keyup', function (event) {
+    if (event.key === 'Escape') {
+      closeAll();
     }
   });
-});
+  
+  // Event Listener for Images
+  moduleImage.forEach(images => {
+    images.addEventListener('click', function (event) {
+      if (clicked === false) {
+        openImage(event.target);
+  
+        clicked = true;
+      } else {
+        closeImageClick(event.target);
+  
+        clicked = false;
+      }
+    });
+  });
+}
+// For clicking and closing modules from images
+modules();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
